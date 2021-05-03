6e08a51c652f369cfbaeabe70cd1b1cdccf42c5e
simonproudfoot <simonproudfoot1980@gmail.com>
2021-05-03 12:05:00 +0100

    heroku setup 3

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+<div id="app">
+    <h1 style="color: #fff">{{received}}</h1>
+    <button @click="stop()">send</button>
+</div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            received: 0,
+            socket: null
+        }
+    },
+
+    methods: {
+        stop() {
+            this.$socket.send('reset');
+        }
+    },
+    mounted() {
+   this.$socket.send('I am the client and I\'m listening!');
+    }
+
+    //     // Create a socket instance
+   
+    //     this.socket = new WebSocket('ws://192.168.0.12:3001/totem');
+
+    //     // Open the socket
+    //     this.socket.onopen = (event) => {
+
+    //         // Send an initial message
+    //         this.socket.send('I am the client and I\'m listening!');
+
+    //         // Listen for messages
+    //         this.socket.onmessage = (event) => {
+    //             console.log('Client received a message', event);
+    //             this.received++
+    //         };
+
+    //         // Listen for socket closes
+    //         this.socket.onclose = (event) => {
+    //             console.log('Client notified socket has closed', event);
+    //         };
+
+    //     };
+
+    //     console.log('sending')
+    //     this.socket.send('message', 'hello');
+    // },
+
+}
+</script>
---
+++ plugins/websocket.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import VueNativeSock from 'vue-native-websocket'
+Vue.use(VueNativeSock, 'ws://192.168.0.12:3001/totem')
