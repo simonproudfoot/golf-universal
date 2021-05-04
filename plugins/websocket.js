305797d2f96fd9c9cc69367030f8930577adb6d7
simonproudfoot <simonproudfoot1980@gmail.com>
2021-05-04 20:24:43 +0100

    websocket ws

--- plugins/websocket.js
+++ plugins/websocket.js
@@ -2,4 +2,4 @@ import Vue from 'vue'
 import VueNativeSock from 'vue-native-websocket'
 
 //Vue.use(VueNativeSock, 'ws://192.168.1.233:3001')
-Vue.use(VueNativeSock, 'wss://socket-mother.herokuapp.com/totem')
+Vue.use(VueNativeSock, 'wss://socket-mother.herokuapp.com')
